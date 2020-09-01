f926b4ee9c5cbe052e3a09513901ef11907f1462
Rion Hutsell <st.gish@gmail.com>
2020-09-01 09:30:47 -0600

    Complete setup and start search index spec

---
+++ spec/search/index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+# As a user,
+# When I visit "/"
+# And I Select "Gryffindor" from the select field
+# (Note: Use the existing select field)
+# And I click "Search For Members"
+# Then I should be on page "/search"
+# Then I should see the total number of the order of the phoenix members that belong to Gryffindor. (i.e. 21)
+# And I should see a list with the detailed information for the 21 members of the Order of the Phoenix for house Gryffindor.
+# And for each of the members I should see:
+# - The name of the member
+# - The members role (if it exists)
+# - The house the member belongs to
+# - The Patronus of the member (if it exists)
+
+RSpec.describe 'Search results index' do
+  it "displays a list of memebers of the Order of the Phoenix when a House is selected on the welcome index" do
+    visit '/'
+
+  end
+end
