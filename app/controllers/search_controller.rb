ffa79b4ce21af2fd1df90c540d09e417f1d0f232
Rion Hutsell <st.gish@gmail.com>
2020-09-01 10:54:50 -0600

    Update controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    house = params[:house]
-    @members = SearchResults.new.members(house)
+    @house = params[:house]
+    @members = SearchResults.new.members(@house)
   end
 end
